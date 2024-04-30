31b285ae276ab4555b1e20266b8f5db4666cbda9
Haler657 <168569183+Haler657@users.noreply.github.com>
2024-04-30 18:50:03 +0300

    vov
    Bobras

---
+++ scratch_4.py
@@ -0,0 +1,13 @@
+def Mega///(x, y):
+    if x > 0 and y > 0:
+        print('1')
+    elif x > 0 and y < 0:
+        print('4')
+    elif x < 0 and Y > 0:
+        print('2')
+    elif x < 0 and y < 0:
+        print('3')
+
+x = int(input())
+y = int(input())
+Mega///(x, y)
